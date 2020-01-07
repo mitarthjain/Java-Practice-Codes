d630b6f1f33a86f5f8f174c148a7c166c638ccc4
mitarthjain <34244395+mitarthjain@users.noreply.github.com>
2020-01-07 19:26:18 +0530

    Update Alpha_Code.java

--- Alpha_Code.java
+++ Alpha_Code.java
@@ -44,3 +44,68 @@ public class Main {
 	}
 
 }
+*********************************************************************************************************************************
+
+import java.util.Scanner;
+ 
+public class Main {
+ 
+	
+	public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        boolean flag=true;
+        while(flag)
+        {
+            String s=sc.next();
+            if(s.equals("0"))
+            {
+                break;
+            }
+            int size=str.length();
+            int arr[]=new int[size];
+            int dp[]=new int[size+1];
+            for(int i=0;i<s.length();i++)
+            {
+                arr[i]=s.charAt(i)-'0';
+            }
+            long sum=subsi(arr,len);
+            System.out.println(sum);
+        }
+		
+ 
+	}
+    public static int subs(int arr[],int size,int dp[])
+    {
+        if(size==0 ||size==1)
+        {
+            return 1;
+        }
+        if(dp[size]>0)
+        {
+            return dp[size];
+        }
+        
+        int output=subs(arr,size-1,dp);
+        if((arr[size-2]*10+arr[size-1])<=26 && (arr[size-2]*10+arr[size-1])>=10)
+        {
+            output+=subs(arr,size-2,dp);
+        }
+        dp[size]=output;
+        return output;
+    }
+    public static long subsi(int arr[],int size)
+    {
+        int dp[]=new int[size+1];
+        dp[0]=dp[1]=1;
+        for(int i=2;i<=size;i++)
+        {
+            if(arr[i-1]!=0)
+            dp[i]+=dp[i-1];
+            int val=arr[i-2]*10+arr[i-1];
+            if(val>=10 && val<=26)
+                dp[i]=(dp[i]+dp[i-2])%1000000007;
+        }
+        return dp[size];
+    }
+ 
+}
