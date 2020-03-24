b87534b7cc94885d767dce4ccf532b77bdd4b032
mitarthjain <34244395+mitarthjain@users.noreply.github.com>
2020-03-24 12:23:45 +0530

    Create Min distance between two given nodes of a Binary Tree.java

---
+++ Min distance between two given nodes of a Binary Tree.java
@@ -0,0 +1,41 @@
+public int findDistance(Node root, int n1, int n2) {
+		int x = Pathlength(root, n1) - 1;
+		int y = Pathlength(root, n2) - 1;
+		int lcaData = findLCA(root, n1, n2).data;
+		int lcaDistance = Pathlength(root, lcaData) - 1;
+		return (x + y) - 2 * lcaDistance;
+	}
+
+	public int Pathlength(Node root, int n1) {
+		if (root != null) {
+			int x = 0;
+			if ((root.data == n1) || (x = Pathlength(root.left, n1)) > 0
+					|| (x = Pathlength(root.right, n1)) > 0) {
+				// System.out.println(root.data);
+				return x + 1;
+			}
+			return 0;
+		}
+		return 0;
+	}
+
+	public Node findLCA(Node root, int n1, int n2) {
+		if (root != null) {
+			if (root.data == n1 || root.data == n2) {
+				return root;
+			}
+			Node left = findLCA(root.left, n1, n2);
+			Node right = findLCA(root.right, n1, n2);
+
+			if (left != null && right != null) {
+				return root;
+			}
+			if (left != null) {
+				return left;
+			}
+			if (right != null) {
+				return right;
+			}
+		}
+		return null;
+	}
