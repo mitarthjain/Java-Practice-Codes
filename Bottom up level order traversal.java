c8790156f3a00d9679c222dd301f5d5217c8b25a
mitarthjain <34244395+mitarthjain@users.noreply.github.com>
2020-04-02 09:52:33 +0530

    Create Bottom up level order traversal.java

---
+++ Bottom up level order traversal.java
@@ -0,0 +1,55 @@
+Leetcode - 107. Binary Tree Level Order Traversal II
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public List<List<Integer>> levelOrderBottom(TreeNode root) {
+        List<List<Integer>> result = new  ArrayList();
+ 
+    if(root == null){
+        return result;
+    }
+ 
+    LinkedList<TreeNode> current = new LinkedList<TreeNode>();
+    LinkedList<TreeNode> next = new LinkedList<TreeNode>();
+    current.offer(root);
+ 
+    List<Integer> numberList = new ArrayList();
+ 
+   
+    while(!current.isEmpty()){
+        TreeNode head = current.poll();
+ 
+        numberList.add(head.val);
+ 
+        if(head.left != null){
+            next.offer(head.left);
+        }
+        if(head.right!= null){
+            next.offer(head.right);
+        }
+ 
+        if(current.isEmpty()){
+            current = next;
+            next = new LinkedList<TreeNode>();
+            result.add(numberList);
+            numberList = new ArrayList<Integer>();
+        }
+    }
+ 
+    
+    List<List<Integer>> reversedResult = new  ArrayList();
+    for(int i=result.size()-1; i>=0; i--){
+        reversedResult.add(result.get(i));
+    }
+ 
+    return reversedResult;
+        
+    }
+}
